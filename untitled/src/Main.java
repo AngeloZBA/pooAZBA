82c71e7ef7b723e5c5cab602b8d23cbdff6561b7
AngeloZBA <abarbaa@est.ups.edu.ec>
2024-01-29 13:06:17 -0500

    Add(III): Add PrintTriangleFunction

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        printSquare(10);
-        System.out.println(" ------------------");
-        printSquare(20);
         //Print square made of stars (*)
         //Length 5 stars x 5 stars
         /*
@@ -12,10 +9,28 @@ public class Main {
         System.out.println("*       *");
         System.out.println("* * * * *");
         */
+        /*
+        printSquare(10);
+        System.out.println(" ------------------");
+        printSquare(20);
+         */
+        printTriangle(6);
+        System.out.println(" ---------------------------------");
+        printTriangle(8);
     }
-    public static void printSquare(int length){
-        for(int i = 1; i <= length; i++){
-            for(int j = 1; j <= length; j++){
+
+    public static void printSquare(int length) {
+        for (int i = 1; i <= length; i++) {
+            for (int j = 1; j <= length; j++) {
+                System.out.print(" *    ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void printTriangle(int base) {
+        for (int i = 0; i <= base - 1; i++) {
+            for (int k = 1; k <= i+1; k++) {
                 System.out.print(" *    ");
             }
             System.out.print("\n");
