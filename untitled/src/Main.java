62f33cfbf61c87588d0ed664205cd13df30a2725
AngeloZBA <abarbaa@est.ups.edu.ec>
2024-01-29 11:32:14 -0500

    Add(II): Add PrintSquareFunction

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printSquare(10);
+        System.out.println(" ------------------");
+        printSquare(20);
+        //Print square made of stars (*)
+        //Length 5 stars x 5 stars
+        /*
+        System.out.println("* * * * *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+        */
+    }
+    public static void printSquare(int length){
+        for(int i = 1; i <= length; i++){
+            for(int j = 1; j <= length; j++){
+                System.out.print(" *    ");
+            }
+            System.out.print("\n");
+        }
     }
 }
